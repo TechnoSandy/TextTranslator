e57e2ab725e955891fbbf3490f5211c5e2b2cd45
Techno Sandy <technosandyindia007@gmail.com>
2017-01-05 18:05:42 +0530

    Added language support for other languages

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
-    function translator(str) {
-        if (str.length == 0) {
-            document.getElementById("text").innerHTML = "";
-            return;
-        }
-        else {
-            var xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange = function () {
-                if (this.readyState == 4 && this.status == 200) {
-                    document.getElementById("translatedText").innerHTML = this.responseText.replace(/<(?:.|\n)*?>/gm, '').trim();
-                }
-            };
-            //xmlhttp.open("GET", "translate.php?q=" + str, true);
-            xmlhttp.open("GET", "http://sandeepbalramsatone.com/texttranslator/translate.php?q=" + str, true);
-            xmlhttp.send();
-        }
+function onchangeHandler() {
+    var numberOfItems = document.getElementById("myForm").elements.length;
+    var textToTranslate = document.getElementById("myForm").elements[0].value;
+    var languageCode = document.getElementById("myForm").elements[1].value;
+    translator(textToTranslate, languageCode);
+    //console.log(textToTranslate);
+    // console.log(languageCode);
+}
+
+function translator(str, langCode) {
+    if (str.length == 0) {
+        document.getElementById("text").innerHTML = "";
+        return;
     }
-    // This will translate from voice to text
-    function startDictation() {
-        if (window.hasOwnProperty('webkitSpeechRecognition')) {
-            var recognition = new webkitSpeechRecognition();
-            recognition.continuous = false;
-            recognition.interimResults = false;
-            recognition.lang = "en-US";
-            recognition.start();
-            recognition.onresult = function (e) {
-                document.getElementById('technosandy').value = e.results[0][0].transcript;
-                recognition.stop();
-                translator(document.getElementById('technosandy').value)
-            };
-            recognition.onerror = function (e) {
-                recognition.stop();
+    else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("translatedText").innerHTML = this.responseText.replace(/<(?:.|\n)*?>/gm, '').trim();
             }
+        };
+        // xmlhttp.open("GET", "translate.php?q=" + str + , true);
+        xmlhttp.open("GET", "http://sandeepbalramsatone.com/texttranslator/translate.php?q=" + str + "&" + "langCode=" + langCode, true);
+        console.log(langCode);
+        console.log(str);
+        xmlhttp.send();
+    }
+}
+// This will translate from voice to text
+function startDictation() {
+    if (window.hasOwnProperty('webkitSpeechRecognition')) {
+        var recognition = new webkitSpeechRecognition();
+        recognition.continuous = false;
+        recognition.interimResults = false;
+        recognition.lang = "en-US";
+        recognition.start();
+        recognition.onresult = function (e) {
+            document.getElementById('technosandy').value = e.results[0][0].transcript;
+            recognition.stop();
+            translator(document.getElementById('technosandy').value)
+        };
+        recognition.onerror = function (e) {
+            recognition.stop();
         }
     }
+}
--- translate.php
+++ translate.php
@@ -2,27 +2,32 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 ?>
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <title></title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
 
-<body>
-    <?php
+    <body>
+        <?php
 require_once('translate.class.php');
 
-$BingTranslator = new BingTranslator('<Application ID>', '<Application Secret>');
+//$BingTranslator = new BingTranslator('<Application ID>', '<Application Secret>');
 
 $q = $_REQUEST["q"];
+        $q = $_REQUEST["q"];
+    $lang = $_REQUEST["langCode"];
+        
     if ($q !== "") {
-    $translation = $BingTranslator->getTranslation('en', 'hi', $q);
+   $translation = $BingTranslator->getTranslation('en',$lang, $q);
 }
-    echo $translation;
+  echo $translation;
+      
+        
 
 ?>
-</body>
+    </body>
 
-</html>
+    </html>
